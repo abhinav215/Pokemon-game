ed2b9ebd3649ad9eb3b95258fe3d8987ef148ed0
Abhinav <abhinav0606june@gmail.com>
2022-06-30 23:39:29 +0530

    reponsive

--- cade2.js
+++ cade2.js
@@ -1,23 +1,23 @@
-let flag=0;
+let flag = 0;
 let pre_number = null;
 
 let box = document.getElementById("box");
-box.onclick = function(){
-  window.location.href="index.html";
-}
+box.onclick = function () {
+  window.location.href = "index.html";
+};
 
 function delay(delayInms) {
-  return new Promise(resolve => {
+  return new Promise((resolve) => {
     setTimeout(() => {
       resolve(2);
     }, delayInms);
   });
 }
 
-function activate(number,pre_number){
-  let allCard =  document.querySelectorAll(".card");
-  for(let i=0; i < allCard.length ; i++){
-    if(allCard[i].classList.contains("active")){
+function activate(number, pre_number) {
+  let allCard = document.querySelectorAll(".card");
+  for (let i = 0; i < allCard.length; i++) {
+    if (allCard[i].classList.contains("active")) {
       allCard[i].classList.remove("active");
     }
   }
@@ -26,77 +26,74 @@ function activate(number,pre_number){
   showing.classList.add("active");
 }
 
-
-async function compare(number,pre_number){
+async function compare(number, pre_number) {
   let showing = document.getElementById(number);
   let showing2 = document.getElementById(pre_number);
   showing.classList.add("active");
-  
+
   let delayres = await delay(1000);
 
   showing.classList.remove("active");
   showing2.classList.remove("active");
-  if((number-pre_number==1) || (number-pre_number==-1)){
+  if (number - pre_number == 1 || number - pre_number == -1) {
     showing.classList.add("hidden");
     showing2.classList.add("hidden");
     showing.classList.remove("exist");
     showing2.classList.remove("exist");
   }
-check_win();
+  check_win();
 }
 
-
-function check_win(){
-  let victory=1
+function check_win() {
+  let victory = 1;
   let option = document.querySelectorAll(".card");
-  for(let i=0;i<option.length; i++){
-    if (option[i].classList.contains("exist")){
-      victory=0
+  for (let i = 0; i < option.length; i++) {
+    if (option[i].classList.contains("exist")) {
+      victory = 0;
     }
   }
-  if (victory == 1){
-    boom()
+  if (victory == 1) {
+    boom();
     clearInterval(samay);
   }
 }
 
-function boom(){
+function boom() {
   let box = document.getElementById("box");
   box.classList.add("aactive");
-  document.querySelector("h1.result").innerHTML = `Congrats :) <br> You Win `;
+  document.querySelector(
+    "h1.result"
+  ).innerHTML = `Congrats <br> :) <br> You Win `;
   document.querySelector("h2.results").innerHTML = `Click to play again`;
 }
 
-function boom2(){
+function boom2() {
   let box = document.getElementById("box");
   box.classList.add("aactive");
-  document.querySelector("h1.result").innerHTML = `Time Up :( <br> You Lose`;
+  document.querySelector(
+    "h1.result"
+  ).innerHTML = `Time Up<br> :( <br> You Lose`;
   document.querySelector("h2.results").innerHTML = `Click to play again`;
 }
 
 // uwin or game over mai ek or page which ask to replay
 
-
 function GFG_click(number) {
-  console.log(number);
-  let clicked =  document.getElementById(number);
-  if(clicked.classList.contains("hidden")){
-    console.log("hidden hai")
-    return
+  // console.log(number);
+  let clicked = document.getElementById(number);
+  if (clicked.classList.contains("hidden")) {
+    // console.log("hidden hai")
+    return;
   }
-  if(flag==0){
-    activate(number,pre_number);
-    pre_number=number;
-    flag=1;
-  }
-  else{
-    if(number!=pre_number){
-      compare(number,pre_number);
-      pre_number = null
-      flag=0;
+  if (flag == 0) {
+    activate(number, pre_number);
+    pre_number = number;
+    flag = 1;
+  } else {
+    if (number != pre_number) {
+      compare(number, pre_number);
+      pre_number = null;
+      flag = 0;
     }
   }
 }
-
-
-
